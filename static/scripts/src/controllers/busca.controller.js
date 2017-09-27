42e68e09b4c3aadd94e580352848f9121511fb52
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-09-27 19:45:31 -0300

    Mapa renderização

--- static/scripts/src/controllers/busca.controller.js
+++ static/scripts/src/controllers/busca.controller.js
@@ -19,6 +19,21 @@
     estados
   ) {
     var self = this;
+    var mapaStyles = [
+      {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }]
+      }
+    ];
+    var optionsMapa = {
+      zoom: 8,
+      center: new google.maps.LatLng(-23.55, -46.63),
+      mapTypeId: 'roadmap',
+      disableDefaultUI: true,
+      zoomControl: true,
+      styles: mapaStyles
+    };
 
     // Models
     self.viewState  = 'filtros';
@@ -27,6 +42,7 @@
     self.categoria  = 1;
     self.filtros    = { Categoria: 1, estado: '', cidade: '' };
     self.listaResultados = [];
+    self.mapa = null;
 
     // Métodos
     self.setViewState = function (state) {
@@ -42,6 +58,8 @@
         .then(function (ofertas) {
           self.listaResultados = angular.copy(ofertas);
         });
+
+      self.iniciarMapa();
     };
 
     self.abrirPerfilComOferta = function (perfil, oferta) {
@@ -52,6 +70,13 @@
       if (categoria > 0 || categoria <= self.categorias.length + 1) {
         self.categoria = categoria;
       }
+    };
+
+    self.iniciarMapa = function () {
+      if (!self.mapa) {
+        self.mapa = new google.maps.Map(document.getElementById('mapa'), optionsMapa);
+      }
+      document.getElementById('mapa').style.height = '100%';
     };
   }
 })();
